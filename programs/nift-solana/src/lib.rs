2e931ddbf2c3e4b29424f2b8f3172a5120701f7e
ylgr <quyen.luminous@gmail.com>
2021-10-13 11:37:23 +0700

    (+) Init

---
+++ programs/nift-solana/src/lib.rs
@@ -0,0 +1,15 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, Mint, TokenAccount, Transfer, MintTo};
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod nift_solana {
+    use super::*;
+    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
